1e4e1444ad9f23b3c58ca828e5a4f7679834c518
jiangjiaqi <jiangjiaqi@duiba.com.cn>
2023-01-16 20:19:39 +0800

    230116-196

--- main.js
+++ main.js
@@ -53,22 +53,30 @@ function cleanup(effectFn) {
     effectFn.deps.length = 0
 }
 
-function effect(fn, options) {
+function effect(fn, options = {}) {
     const effectFn = () => {
         cleanup(effectFn)
         activeEffect = effectFn
         effectStack.push(effectFn)
-        fn()
+        const res = fn()
         effectStack.pop()
         activeEffect = effectStack[effectStack.length - 1]
+        return res
     }
     effectFn.options = options
     effectFn.deps = []
-    effectFn()
+    if (!options.lazy) {
+        effectFn()
+    }
+    return effectFn
 }
 
+const TriggerType = {
+    SET: 'SET',
+    ADD: 'ADD'
+}
 function track(target, key) {
-    if (!activeEffect) return
+    if (!activeEffect || !shouldTrack) return
 
     let depsMap = bucket.get(target)
     if (!depsMap) {
@@ -81,10 +89,11 @@ function track(target, key) {
     deps.add(activeEffect)
     activeEffect.deps.push(deps)
 }
-function trigger(target, key) {
+function trigger(target, key, type, newVal) {
     const depsMap = bucket.get(target)
     if (!depsMap) return
     const effects = depsMap.get(key)
+    const iterateEffects = depsMap.get(ITERATE_KEY)
 
     const effectsToRun = new Set()
     effects && effects.forEach(effectFn => {
@@ -92,6 +101,32 @@ function trigger(target, key) {
             effectsToRun.add(effectFn)
         }
     })
+    if (type === 'ADD' || type === 'DELETE') {
+        iterateEffects && iterateEffects.forEach(effectFn => {
+            if (effectFn !== activeEffect) {
+                effectsToRun.add(effectFn)
+            }
+        })
+    }
+    if (type === 'ADD' && Array.isArray(target)) {
+        const lengthEffects = depsMap.get('length')
+        lengthEffects && lengthEffects.forEach(effectFn => {
+            if (effectFn !== activeEffect) {
+                effectsToRun.add(effectFn)
+            }
+        })
+    }
+    if (Array.isArray(target) && key === 'length') {
+        depsMap.forEach((effects, key) => {
+            if (key > newVal) {
+                effects.forEach(effectFn => {
+                    if (effectFn !== activeEffect) {
+                       effectsToRun.add(effectFn)
+                    }
+                })
+            }
+        })
+    }
     effectsToRun.forEach(effectFn => {
         if (effectFn.options.scheduler) {
             effectFn.options.scheduler(effectFn)
@@ -101,12 +136,93 @@ function trigger(target, key) {
     })
 }
 
+function computed(getter) {
+    let value
+    let dirty = true
+
+    const effectFn = effect(getter, {
+        lazy: true,
+        scheduler() {
+            if (!dirty) {
+                dirty = true
+                trigger(obj, 'value')
+            }
+        }
+    })
+
+    const obj = {
+        get value() {
+            if (dirty) {
+                value = effectFn()
+                dirty = false
+            }
+            track(obj, 'value')
+            return value
+        }
+    }
+    return obj
+}
+
+function watch(source, cb, options = {}) {
+    let getter
+    if (typeof source === 'function') {
+        getter = source
+    } else {
+        getter = () => traverse(source)
+    }
+    let oldValue, newValue
+
+    let cleanup
+    function onInvalidate(fn) {
+        cleanup = fn
+    }
+
+    const job = () => {
+        newValue = effectFn()
+        if (cleanup) {
+            cleanup()
+        }
+        cb(newValue, oldValue, onInvalidate)
+        oldValue = newValue
+    }
+
+    const effectFn = effect(
+        () => getter(),
+        {
+            lazy: true,
+            scheduler: () => {
+                if (options.fulsh === 'post') {
+                    const p = Promise.resolve()
+                    p.then(job)
+                } else {
+                    job()
+                }
+            }
+        }
+    )
+
+    if (options.immediate) {
+        job()
+    } else {
+        oldValue = effectFn()
+    }
+}
+
+function traverse(value, seen = new Set()) {
+    if (typeof value !== 'object' || value === null || seen.add(value)) return
+    seen.add(value)
+    for (const k in value) {
+        traverse(value[k], seen)
+    }
+    return value
+}
+
 const jobQueue = new Set()
 const p = Promise.resolve()
 
 let isFlushing = false
 function flushJob() {
-    if(isFlushing) return
+    if (isFlushing) return
     isFlushing = true
     p.then(() => {
         jobQueue.forEach(job => job())
@@ -116,29 +232,134 @@ function flushJob() {
 }
 
 
-const data = { foo: 1 }
+const ITERATE_KEY = Symbol()
 
-const obj = new Proxy(data, {
-    get(target, key) {
-        track(target, key)
-        return target[key]
-    },
-    set(target, key, newVal) {
-        target[key] = newVal
-        trigger(target, key)
+const originMethod = Array.prototype.includes
+const arrayInstrumentations = {
+    includes: function(...args) { 
+        let res = originMethod.apply(this, args)
+
+        if (res === false) {
+            res = originMethod.apply(this.raw, args)
+        }
+        return res
+    }
+}
+const mutableInstrumentations = {
+    add(key) {
+        const target = this.raw
+        const res = target.add(key)
+        trigger(target, key, 'ADD')
+        return res
+    }
+}
+
+let shouldTrack = true
+;['push','pop','shift','unshift','splice'].forEach(method => {
+    const originMethod = Array.prototype[method]
+    arrayInstrumentations[method] = function(...args) {
+        shouldTrack = false
+        let res = originMethod.apply(this, args)
+        shouldTrack = true
+        return res
     }
 })
+
+const reactiveMap = new Map()
+
+function reactive(obj) {
+    const existionProxy = reactiveMap.get(obj)
+    if (existionProxy) return existionProxy
+
+    const proxy = createReactive(obj)
+    reactiveMap.set(obj, proxy)
+    return proxy
+}
+
+function createReactive(obj, isShallow = false, isReadonly = false) {
+    return new Proxy(obj, {
+        get(target, key, receiver) {
+            if (key === 'raw') {
+                return target
+            }
+            if (key === 'size') {
+                track(target, ITERATE_KEY)
+                return Reflect.get(target, key, target)
+            }
+            return mutableInstrumentations[key]
+
+            if (Array.isArray(target) && arrayInstrumentations.hasOwnProperty(key)) {
+                return Reflect.get(arrayInstrumentations, key, receiver)
+            }
+            if (!isReadonly && typeof key !== 'symbol') {
+                track(target, key)
+            }
+            const res = Reflect.get(target, key, receiver)
+            if(isShallow) {
+                return res
+            }
+            if (typeof res === 'object' && res !== null) {
+                return isReadonly ? readonly(res): reactive(res)
+            }
+            return res
+        },
+        has(target, key) {
+            track(target, key)
+            return Reflect.has(target, key)
+        },
+        ownKeys(target) {
+            track(target, Array.isArray(target) ? 'length' : ITERATE_KEY)
+            return Reflect.ownKeys(target)
+        },
+        set(target, key, newVal, receiver) {
+            if (isReadonly) {
+                console.warn(`属性${key}是只读的`)
+                return true
+            }
+            const oldVal = target[key]
+            const type =  Array.isArray(target)
+                ? Number(key) < target.length ? 'SET' : 'ADD'
+                : Object.prototype.hasOwnProperty.call(target, key) ? 'SET' : 'ADD'
+            const res = Reflect.set(target, key, newVal, receiver)
+            if (target === receiver.raw) {
+                if (oldVal !== newVal && (oldVal === oldVal || newVal === newVal)) {
+                    trigger(target, key, type, newVal)
+                }
+            }
+            return res
+        },
+        deleteProperty(target, key) {
+            if (isReadonly) {
+                console.warn(`属性${key}是只读的`)
+                return true
+            }
+            const hadKey = Object.prototype.hasOwnProperty.call(target, key)
+            const res = Reflect.deleteProperty(target, key)
+
+            if (res && hadKey) {
+                trigger(target, key, 'DELETE')
+            }
+            return res
+        }
+    })
+}
+
+function shallowReactive(obj) {
+    return createReactive(obj, true)
+}
+
+function readonly(obj) { 
+    return createReactive(obj, false, true)
+}
+
+function shallowReadonly(obj) {
+    return createReactive(obj, true, true)
+}
+
+const p1 = reactive(new Set(1, 2, 3))
 
 effect(() => {
-    console.log(obj.foo)
-},
-{
-    scheduler(fn) {
-        jobQueue.add(fn)
-        flushJob()
-    }
+    console.log(p1.size)
 })
 
-obj.foo++
-obj.foo++
-
+p1.add(1)
